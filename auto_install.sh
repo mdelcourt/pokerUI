21eedec0af9c6ffbbdd450971e63be6649901c56
mdelcourt <martin.delcourt@uclouvain.be>
2015-11-01 19:31:56 +0100

    minor change

--- auto_install.sh
+++ auto_install.sh
@@ -1,5 +1,6 @@
 chmod +x pokerUI.py
 echo "*** Checking dependencies"
+sudo apt-get update
 sudo apt-get install g++ libsdl-ttf2.0-dev libsdl1.2-dev python-tk libsdl-image1.2-dev
 cd blindes/files
 chmod +x ../launcher.sh
